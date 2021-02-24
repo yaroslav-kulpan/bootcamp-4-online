e2092614ad8dc39832e0eabba84896a588bffdf6
Kylpan21 <yaroslav.kylpan@gmail.com>
2021-02-24 09:57:07 +0200

    +++

--- lesson-1-2-practics/src/components/App/App.jsx
+++ lesson-1-2-practics/src/components/App/App.jsx
@@ -1,13 +1,11 @@
 function App() {
   return (
-    <div className="App">
       <h1>Title</h1>
       <h1>SubTitle</h1>
       <h1>SubTitle</h1>
       <h1>SubTitle</h1>
       <h1>SubTitle</h1>
       <h1>SubTitle</h1>
-    </div>
   );
 }
 
