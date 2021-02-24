6b6f91aad244d15d289517d6f2906d4c7c65b0ba
Yaroslav <yaroslav.kulpan.dev@gmail.com>
2021-02-24 09:51:19 +0200

    +++

--- lesson-1-2-practics/src/components/App/App.jsx
+++ lesson-1-2-practics/src/components/App/App.jsx
@@ -1,11 +1,22 @@
 function App() {
   return (
     <div className="App">
-<h1>Title</h1>
-<h1>SubTitle</h1>
-<h1>SubTitle</h1>
-<h1>SubTitle</h1>
-<h1>SubTitle</h1>
+      <h1>Title</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
+      <h1>SubTitle</h1>
     </div>
   );
 }
