bf0afc8ff0424d041d21220a8ab0396b84c02dbb
Yaroslav <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-04-01 00:32:12 +0300

    +++

--- hooks/src/components/swapi/Peoples/Peoples.jsx
+++ hooks/src/components/swapi/Peoples/Peoples.jsx
@@ -2,17 +2,11 @@ import React, { useState } from "react";
 import useFetch from "../../../hooks/use-fetch";
 
 const Peoples = () => {
-  // const [next, setNext] = useState(null);
-  // const [prev, setPrev] = useState(null);
   const [page, setPage] = useState(1);
   const { data, loading, error } = useFetch(
     `http://swapi.dev/api/people/?page=${page}`,
-      true
+    true
   );
-  // console.log(data)
-  // const prevPage = () => {
-  //   setPage(page - 1);
-  // };
 
   const nextPage = () => {
     setPage(page + 1);
@@ -25,7 +19,6 @@ const Peoples = () => {
       ))}
       {loading && <p>Loading...</p>}
       {error && <p>Errors</p>}
-      {/*<button onClick={prevPage}>Prev</button>*/}
       <button onClick={nextPage}>ShowMore</button>
     </ul>
   );
